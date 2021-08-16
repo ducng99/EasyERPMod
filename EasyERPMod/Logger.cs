a9acd6e9c891e2b6cf3ab0d82c9e94f76af10bea
Tom <Tom@ROSIEMACHINE>
2021-08-16 16:00:46 +1200

    Add project files.

---
+++ EasyERPMod/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyERPMod
+{
+    static class Logger
+    {
+        public static void Log(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("[-] " + msg);
+        }
+
+        public static void Warning(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("[*] " + msg);
+        }
+
+        public static void Error(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[X] " + msg);
+        }
+    }
+}
---
+++ EasyERPMod/Models/ResourcesModel.cs
@@ -0,0 +1,60 @@
+﻿using EgoEngineLibrary.Archive.Erp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyERPMod.Models
+{
+    class ResourcesModel
+    {
+        private const string QEscString = "%3F";
+        private const string FragNameDelim = "!!!";
+
+        public List<ErpResource> Resources { get; private set; }
+
+        public ResourcesModel(ErpFile data)
+        {
+            Resources = data.Resources;
+        }
+        /*
+        public void Export(string folderPath)
+        {
+            foreach (var resource in Resources)
+            {
+                try
+                {
+                    var outputDir = Path.Combine(folderPath, resource.Folder);
+                    Directory.CreateDirectory(outputDir);
+
+                    for (var i = 0; i < resource.Fragments.Count; ++i)
+                    {
+                        var fragment = resource.Fragments[i];
+                        var fileName = GetFragmentFileName(resource, fragment, i);
+                        var filePath = Path.Combine(outputDir, fileName);
+
+                        using var fs = File.Open(filePath, FileMode.Create, FileAccess.Write, FileShare.Read);
+                        using var decompressStream = fragment.GetDecompressDataStream(true);
+                        decompressStream.CopyTo(fs);
+                    }
+                }
+                catch (Exception)
+                {
+                    Logger.Error("Failed to export " + resource.FileName);
+                }
+            }
+        }
+
+        private static string GetFragmentFileName(ErpResource resource, ErpFragment fragment, int fragmentIndex)
+        {
+            var name = resource.FileName;
+            name = name.Replace("?", QEscString);
+            name = Path.GetFileNameWithoutExtension(name) + FragNameDelim
+                + fragment.Name + fragmentIndex.ToString("000")
+                + Path.GetExtension(name);
+            return name;
+        }*/
+    }
+}
